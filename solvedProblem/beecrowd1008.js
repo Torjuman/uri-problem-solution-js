b03e73cc50d847ca0d12bdb2c5de39535b78d623
Torjuman <torjumanrimon7@gmail.com>
2022-01-03 23:25:28 +0600

    problem number 1008 solved

---
+++ solvedProblem/beecrowd1008.js
@@ -0,0 +1,6 @@
+let a = parseInt(readline()),
+  b = parseInt(readline()),
+  c = parseFloat(readline()),
+  salary = parseFloat(b * c);
+print("NUMBER = " + a);
+print("SALARY = U$ " + salary.toFixed(2));
