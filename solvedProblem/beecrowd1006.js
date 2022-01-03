ddc476fb95516ef184a57f9685a42c1c40e4472a
Torjuman <torjumanrimon7@gmail.com>
2022-01-03 22:53:59 +0600

    problem number 1006 solved

---
+++ solvedProblem/beecrowd1006.js
@@ -0,0 +1,7 @@
+let a = parseFloat(readline()),
+  b = parseFloat(readline()),
+  c = parseFloat(readline());
+
+let av = (a * 2 + b * 3 + c * 5) / (2 + 3 + 5);
+
+print("MEDIA = " + av.toFixed(1));
