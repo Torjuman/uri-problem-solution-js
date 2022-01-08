6700d64ac58f043203c4dfa46552c214f26e9101
Torjuman <torjumanrimon7@gmail.com>
2022-01-08 22:57:58 +0600

    problem number 1020 solved

--- solvedProblem/beecrowd1020.js
+++ solvedProblem/beecrowd1020.js
@@ -1,9 +1,9 @@
-let n = 800,
+let n = parseInt(readline()),
   arr = [365, 30],
   years = parseInt(n / arr[0]),
   months = parseInt((n % arr[0]) / arr[1]),
   days = (n % arr[0]) % arr[1];
 
-console.log(years + " ano(s)");
-console.log(months + " mes(es)");
-console.log(days + " dia(s)");
+print(years + " ano(s)");
+print(months + " mes(es)");
+print(days + " dia(s)");
