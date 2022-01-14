5855533ac2f924facecbffc38c0eff03be28a516
Torjuman <torjumanrimon7@gmail.com>
2022-01-14 20:42:09 +0600

    template has modified

--- template.js
+++ template.js
@@ -1,13 +1,13 @@
 "use strict";
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
- 
+
 function print(x) {
   console.log(x);
 }
 let inputString = "";
 let currentLine = 0;
- 
+
 process.stdin.on("data", (inputStdin) => {
   inputString += inputStdin;
 });
@@ -18,7 +18,7 @@ process.stdin.on("end", () => {
 function readline() {
   return inputString[currentLine++];
 }
- 
+
 // ********** Code Start **********
 
 function main() {
