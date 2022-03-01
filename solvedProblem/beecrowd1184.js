59fd8cac1465333ea9a1e069eab7a0874a78d5b7
Torjuman <torjumanrimon7@gmail.com>
2022-03-01 13:07:55 +0600

    Problem number 1184 solved

---
+++ solvedProblem/beecrowd1184.js
@@ -0,0 +1,22 @@
+let i,
+  j,
+  k,
+  sum = 0.0,
+  char = readline();
+
+for (i = 0; i < 12; i++) {
+  let temArr = [];
+  for (j = 0; j < 12; j++) {
+    let n = parseFloat(readline());
+    temArr.push(n);
+  }
+  for (k = 0; k < i; k++) {
+    sum += temArr[k];
+  }
+}
+if (char === "S") {
+  console.log(sum.toFixed(1));
+} else if (char === "M") {
+  let m = sum / 66.0;
+  console.log(m.toFixed(1));
+}
