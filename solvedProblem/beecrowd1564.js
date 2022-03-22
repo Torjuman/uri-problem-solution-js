02ecef5ea1a0c53b227dea30935865bc15673853
Torjuman <torjumanrimon7@gmail.com>
2022-03-22 13:14:41 +0600

    Problem number 1564 solved

---
+++ solvedProblem/beecrowd1564.js
@@ -0,0 +1,10 @@
+while (true) {
+  let n = parseInt(readline());
+  if (n === 0) {
+    console.log("vai ter copa!");
+  } else if (n > 0) {
+    console.log("vai ter duas!");
+  } else {
+    break;
+  }
+}
